13c76a6f5cfc777cc82e9adff84fa70d6b1061de
Harshwardhan <Harshwardhanw.tagline@gmail.com>
2023-02-21 10:16:22 +0530

    Js: Code optimization is done (Dynamic object added)

--- state_city.js
+++ state_city.js
@@ -1,41 +1,43 @@
+
+const data = { 
+    "india": {
+      "gujarat": ["Surat", "Rajkot", "Jamnagar", "Vadodara"],
+      "up": ["Kanpur", "Agra", "Noida", "Saharanpur"],
+      "dehli": ["Gokalpuri", "New-Dehli", "Mandoli", "Taj-Pul"]
+    },
+    "usa": { 
+      "texas": ["Austin", "SanAntonio", "Dallas", "Houston"],
+      "chicago": ["Lyons", "ForestPark", "RiverForest", "Lincolnwood"],
+      "lasVegas": ["SpringValley", "Boulder", "Paradise", "Henderson"]
+    }
+  };
 function getCountryValue(){
     let countryDropdown = document.getElementById("country").value;
     var stateDropdown = document.getElementById("states");
     stateDropdown.innerHTML = ""
-    if(countryDropdown.toLowerCase()=="india"){
-        var states = ["Gujarat", "UP", "Dehli"]
-    }else if(countryDropdown.toLowerCase()=="usa"){
-        var states = ["LasVegas", "Chicago", "Texas"]
+    if (data.hasOwnProperty(countryDropdown)){
+        var states = Object.keys(data[countryDropdown]);
     }
-
     for (var index = 0; index < states.length; index++) {
         var option = document.createElement("option");
-        option.text = states[index];
+        option.text = states[index].toUpperCase();
         stateDropdown.add(option);
       }
 }
 
 function getStateValue(){
-    let stateDropdown = document.getElementById("states").value;
+    let countryDropdown = document.getElementById("country").value;
+    let stateDropdown = document.getElementById("states").value.toLowerCase();
     var cityDropdown = document.getElementById("city");
     cityDropdown.innerHTML = ""
-    if (stateDropdown.toLowerCase()=="gujarat"){
-        var cities = ["Surat", "Rajkot", "Jamnagar", "Vadodara"]
-    }else if(stateDropdown.toLowerCase()=="dehli"){
-        var cities = ["Gokalpuri", "New-Dehli", "Mandoli", "Taj-Pul"]
-    }else if(stateDropdown.toLowerCase()=="up"){
-        var cities = ["Kanpur", "Agra", "Noida", "Saharanpur"]
-    }else if (stateDropdown.toLowerCase()=="chicago"){
-        var cities = ["Lyons", "ForestPark", "RiverForest", "Lincolnwood"]
-    }else if (stateDropdown.toLowerCase()=="texas"){
-        var cities = ["Austin", "SanAntonio", "Dallas", "Houston"]
-    }else if (stateDropdown.toLowerCase()=="lasvegas"){
-        var cities = ["SpringValley", "Boulder", "Paradise", "Henderson"]
-    }
+
+    if (data[countryDropdown].hasOwnProperty(stateDropdown)){
+        var cities = data[countryDropdown][stateDropdown];
+    };
 
     for (var index = 0; index < cities.length; index++) {
         var option = document.createElement("option");
-        option.text = cities[index];
+        option.text = cities[index].toUpperCase();
         cityDropdown.add(option);
       }
 }
